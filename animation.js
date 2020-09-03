d33f6d02924346e3397e5f270bc1f452bab33c34
Bertrand Jouin <bertrandjouin@gmail.com>
2020-09-03 16:43:37 +0200

    saispastrop ce que je fais

--- animation.js
+++ animation.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded',function(event){
   // array with texts to type in typewriter
   var dataText = ["Spoutnik Conseil"];
-
   // type one text in the typwriter
   // keeps calling itself until the text is finished
   function typeWriter(text, i, fnCallback) {
@@ -40,3 +39,4 @@ document.addEventListener('DOMContentLoaded',function(event){
   // start the text animation
   StartTextAnimation(0);
 });
+
